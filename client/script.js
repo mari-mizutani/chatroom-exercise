1695c0c64b965f1162a0520723800f58f29f872d
mari-mizutani <82095084+mari-mizutani@users.noreply.github.com>
2021-08-12 11:10:43 +0200

    installed counter on screen

--- client/script.js
+++ client/script.js
@@ -4,15 +4,22 @@ let message = document.getElementById('message');
 let sendBtn = document.getElementById('send');
 let keepBtn = document.getElementById('keep');
 let targetArea = document.getElementById('messages');
+let showCounter = document.getElementById("counter");
 
 socket.on("displayMessage",(message)=>{ //catch the message which server sends on client
     targetArea.innerHTML+=message+"<br>";
-})
+});
 
 sendBtn.addEventListener("click",()=>{
  socket.emit("sendToAll",message.value); //send data to server
-})
+});
 
 keepBtn.addEventListener("click",()=>{
     socket.emit("sendToMe",message.value); //send data to yourself
-})
+});
+
+
+// Counter on the screen
+socket.on("usercnt", (counter) => {
+    showCounter.innerHTML=counter;
+});
